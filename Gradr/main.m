ac7a0621734a0fa40ae88c0114709c7181311796
Sarat Tallamraju <sarattallamraju@gmail.com>
2016-10-27 14:28:04 -0700

    Initial commit

---
+++ Gradr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Gradr
+//
+//  Created by Sarat Tallamraju on 10/27/16.
+//  Copyright © 2016 Sarat Tallamraju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
