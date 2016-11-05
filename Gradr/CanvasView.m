526634dc8adc306667b160be46836a207061bea3
Sarat Tallamraju <sarattallamraju@gmail.com>
2016-11-05 11:38:19 -0700

    Added a CanvasView

---
+++ Gradr/CanvasView.m
@@ -0,0 +1,21 @@
+//
+//  CanvasView.m
+//  Gradr
+//
+//  Created by Sarat Tallamraju on 11/5/16.
+//  Copyright © 2016 Sarat Tallamraju. All rights reserved.
+//
+
+#import "CanvasView.h"
+
+@implementation CanvasView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
