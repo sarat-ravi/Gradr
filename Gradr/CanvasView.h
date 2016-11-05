36fe3b724019db47e1e05ce33ee7c7e84f8f3c91
Sarat Tallamraju <sarattallamraju@gmail.com>
2016-11-05 11:38:19 -0700

    Added a CanvasView

---
+++ Gradr/CanvasView.h
@@ -0,0 +1,13 @@
+//
+//  CanvasView.h
+//  Gradr
+//
+//  Created by Sarat Tallamraju on 11/5/16.
+//  Copyright © 2016 Sarat Tallamraju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CanvasView : UIView
+
+@end
