2a7e72ee472c984e4f9a7f5cf32cdedd2439ea05
Sarat Tallamraju <sarattallamraju@gmail.com>
2016-11-05 12:21:06 -0700

    Added pan gesture recognizer to CanvasView

--- Gradr/CanvasView.m
+++ Gradr/CanvasView.m
@@ -17,5 +17,46 @@
     // Drawing code
 }
 */
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self initComponents];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder: aDecoder];
+    if (self) {
+        [self initComponents];
+    }
+    return self;
+}
+
+- (void)initComponents {
+    UINib *nib = [UINib nibWithNibName: @"CanvasView" bundle:nil];
+    [nib instantiateWithOwner: self options:nil];
+    
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onDrawing:)];
+    [self addGestureRecognizer:panGesture];
+}
+
+- (IBAction)onDrawing:(UIPanGestureRecognizer *)sender {
+    switch (sender.state) {
+        case UIGestureRecognizerStateBegan:
+            NSLog(@"Started");
+            break;
+        case UIGestureRecognizerStateChanged:
+            NSLog(@"Changed");
+            CGPoint delta = [sender translationInView: self];
+            break;
+        case UIGestureRecognizerStateEnded:
+            NSLog(@"Ended");
+            CGPoint velocity = [sender velocityInView: self];
+            break;
+        default:
+            break;
+    }
+}
 
 @end
